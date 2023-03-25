1299d16e8b921ac7086759275125abcc65e591a7
Berker Gungor <yasarberkergungor@gmail.com>
2023-03-25 04:12:10 +0300

    feat: add solution to exercise 08

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,21 +2,58 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, renderHook} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
+const Wrapper = ({initialCount, step}) => {
+  const {count, increment, decrement} = useCounter({initialCount, step})
 
-test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  return (
+    <>
+      <div>Count: {count}</div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </>
+  )
+}
+
+test('initialCount and step', async () => {
+  render(<Wrapper initialCount={3} step={2} />)
+
+  const count = screen.getByText(/count/i)
+  const incrementBtn = screen.getByRole('button', {name: /increment/i})
+  const decrementBtn = screen.getByRole('button', {name: /decrement/i})
+
+  expect(count).toHaveTextContent('Count: 3')
+
+  await userEvent.click(incrementBtn)
+  expect(count).toHaveTextContent('Count: 5')
+
+  await userEvent.click(incrementBtn)
+  expect(count).toHaveTextContent('Count: 7')
+
+  await userEvent.click(decrementBtn)
+  expect(count).toHaveTextContent('Count: 5')
+})
+
+test('exposes the count and increment/decrement functions', async () => {
+  render(<Wrapper />)
+
+  const count = screen.getByText(/count/i)
+  const incrementBtn = screen.getByRole('button', {name: /increment/i})
+  const decrementBtn = screen.getByRole('button', {name: /decrement/i})
+
+  expect(count).toHaveTextContent('Count: 0')
+
+  await userEvent.click(incrementBtn)
+  expect(count).toHaveTextContent('Count: 1')
+
+  await userEvent.click(incrementBtn)
+  expect(count).toHaveTextContent('Count: 2')
+
+  await userEvent.click(decrementBtn)
+  expect(count).toHaveTextContent('Count: 1')
 })
 
 /* eslint no-unused-vars:0 */
